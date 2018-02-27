3e25a5c8126ff01cb4d07d6bba1818ff2ed57146
duyhung0206 <hung02061994@gmail.com>
2018-02-28 00:08:00 +0700

    master

--- packages/hades/bms/src/BmsServiceProvider.php
+++ packages/hades/bms/src/BmsServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Hades\Bms;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,7 +13,25 @@ class BmsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (! $this->app->routesAreCached()) {
+            require __DIR__.'/app/Http/routes.php';
+        }
+
+        /*load views*/
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'bms');
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang/en', 'bms');
+
+        $this->publishes([
+            __DIR__.'/public' => public_path(),
+        ], 'public');
+
+        $this->publishes([
+            __DIR__.'/config' => config_path()
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/database/migrations/' => database_path('migrations')
+        ], 'migrations');
     }
 
     /**
